eaed633adb854dbb9d8055409e5a7871f6b69244
Riddick <87519338+danijelgalic@users.noreply.github.com>
2024-11-06 10:03:45 +0100

    Create Arr.php

---
+++ Arr.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Web Application Framework package
+ *
+ * This is a PHP-CS-Fixer configuration file.
+ * This will help align the code style throughout the project following
+ * specific rules such as 'no_trailing_whitespace', 'no_empty_phpdoc', 'no_superfluous_phpdoc_tags' and many others.
+ * This brings consistency to coding style and makes the code easier to read and understand.
+ * @since 1.0.0
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2025 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package Core
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+class Arr {
+
+  public function groupBy ():void
+  {
+    
+  }
+  
+}
